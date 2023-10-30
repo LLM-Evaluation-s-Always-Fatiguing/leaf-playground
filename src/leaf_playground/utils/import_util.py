88cd073fa0ff9f99740bc48fac4026047a573975
panqiwei <panqiwei@shanda.com>
2023-10-30 17:37:11 +0800

    add a simple import util

---
+++ src/leaf_playground/utils/import_util.py
@@ -0,0 +1,28 @@
+import importlib
+import importlib.machinery
+from types import ModuleType
+from typing import Optional, Any
+
+from pydantic import BaseModel, Field, FilePath
+
+
+class DynamicObject(BaseModel):
+    obj: str = Field(default=...)
+    module: Optional[str] = Field(default=None)
+    source_file: Optional[FilePath] = Field(default=None)
+
+    def model_post_init(self, __context: Any) -> None:
+        if self.module is None and self.source_file is None:
+            raise ValueError("at least one of 'module' or 'source_file' should not None.")
+        if self.module and self.source_file:
+            raise ValueError("can't specify 'module' and 'source_file' at the same time.")
+
+
+def dynamically_import_obj(o: DynamicObject):
+    if o.module is not None:
+        module = importlib.import_module(o.module)
+    else:
+        module = importlib.machinery.SourceFileLoader(
+            o.source_file.name, o.source_file.as_posix()
+        ).load_module()
+    return module.__dict__[o.obj]
