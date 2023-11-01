58301a1b62101d71cd8b0b385757cd5bbbb60087
panqiwei <panqiwei@shanda.com>
2023-11-01 16:09:32 +0800

    add setup.py and set version to 0.0.1.dev0

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, Extension, find_packages
+
+version = "0.0.1.dev0"
+
+requirements = [
+    "pydantic>=2.0.0",
+    "datasets",
+    "openai"
+]
+
+setup(
+    name="leaf_playground",
+    version=version,
+    install_requires=requirements,
+    package_dir={"": "src"},
+    packages=find_packages("src"),
+    python_requires=">=3.8.0"
+)
