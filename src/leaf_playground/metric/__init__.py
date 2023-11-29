b6470097af6a8b15c98e9ca5cb0a4609221c2303
panqiwei <panqiwei@shanda.com>
2023-11-29 16:27:36 +0800

    initial implementation of echart auto generation

---
+++ src/leaf_playground/metric/__init__.py
@@ -0,0 +1,18 @@
+def simple_agg_sum(values):
+    return sum(values)
+
+
+def simple_agg_mean(values):
+    return sum(values) / len(values)
+
+
+def simple_agg_max(values):
+    return max(values)
+
+
+def simple_agg_min(values):
+    return min(values)
+
+
+def simple_agg_median(values):
+    return sorted(values)[len(values) // 2]
