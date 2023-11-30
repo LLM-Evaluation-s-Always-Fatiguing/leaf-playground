a8f8816d2010c919c3d2d4ee18204841b706245a
panqiwei <panqiwei@shanda.com>
2023-11-30 15:53:51 +0800

    revert exclude Profile's id field

--- src/leaf_playground/data/profile.py
+++ src/leaf_playground/data/profile.py
@@ -19,7 +19,7 @@ class Role(Data):
 
 
 class Profile(Data):
-    id: UUID = Field(default_factory=lambda: uuid4(), exclude=True)
+    id: UUID = Field(default_factory=lambda: uuid4())
     name: str = Field(default=...)
     role: Optional[Role] = Field(default=None)
 
