3808f67912183becfc208174059415b76ef0b13e
panqiwei <panqiwei@shanda.com>
2023-12-12 20:21:28 +0800

    save changes but not everything done

--- src/leaf_playground/app/const.py
+++ src/leaf_playground/app/const.py
@@ -3,7 +3,7 @@ import os
 
 ROOT = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 
-ZOO_ROOT = os.path.join(ROOT, "zoo")
+ZOO_ROOT = os.path.join(ROOT, "zoo_new")  # TODO: change back to zoo when everything done
 
 SAVE_ROOT = os.path.join(os.getcwd(), "output")
 
