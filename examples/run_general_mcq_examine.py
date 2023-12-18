fa3f2dcdd9a505f4aa82a2fca06d7238c66e6601
panqiwei <panqiwei@shanda.com>
2023-12-18 11:16:20 +0800

    fix wrong dir name

--- examples/run_general_mcq_examine.py
+++ examples/run_general_mcq_examine.py
@@ -65,7 +65,7 @@ def display_log(log: GeneralMCQExamineScene.log_body_class):
 async def run():
     scene_engine = init_scene_engine()
     await asyncio.gather(scene_engine.a_run(), scene_engine.stream_logs(display_log))
-    scene_engine.save_dir = f"output/rag_qa_result-{scene_engine.id.hex}"
+    scene_engine.save_dir = f"output/{scene_engine.id.hex}"
     scene_engine.save()
 
 
