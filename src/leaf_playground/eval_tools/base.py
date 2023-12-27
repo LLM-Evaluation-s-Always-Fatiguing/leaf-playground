1260820fb2131d6524e0331290eca68f0fd13ffc
Pandazki <onlrrr@gmail.com>
2023-12-28 01:24:28 +0800

    feat: add simple bar and general open eval

--- src/leaf_playground/eval_tools/base.py
+++ src/leaf_playground/eval_tools/base.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from .._config import _Config, _Configurable
 
 
@@ -8,6 +10,10 @@ class EvalToolConfig(_Config):
 class EvalTool(_Configurable):
     config_cls = EvalToolConfig
     config: config_cls
+    activated_metrics: List[str]
+
+    def set_activated_metrics(self, metrics: List[str]):
+        self.activated_metrics = metrics
 
     @classmethod
     def from_config(cls, config: config_cls) -> "EvalTool":
