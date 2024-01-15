dee9eef54c446aeadf4bfea025265b62ff2e700e
PanQiWei <594557445@qq.com>
2024-01-15 17:27:34 +0800

    refactor: refactor SocketHandler as an abstract class and leave implementation of its methods to application layer.

--- src/leaf_playground/core/workers/socket_handler.py
+++ src/leaf_playground/core/workers/socket_handler.py
@@ -1,52 +1,28 @@
-import asyncio
+from abc import abstractmethod
 
 from fastapi import WebSocket
 
 from .logger import LogHandler
-from ...data.log_body import LogBody, ActionLogBody
+from ...data.log_body import LogBody
 from ...data.message import MessagePool
-from ...data.socket_data import SocketData, SocketOperation
 
 
 class SocketHandler(LogHandler):
     def __init__(self):
         self._stopped = False
-
         self._message_pool: MessagePool = MessagePool.get_instance()
-        self._socket_cache = []
 
-    def notify_create(self, log_body: LogBody):
-        log_dict = log_body.model_dump(mode="json")
-        if isinstance(log_body, ActionLogBody):
-            log_dict["response"] = self._message_pool.get_message_by_id(log_body.response).model_dump(mode="json")
-        self._socket_cache.append(
-            SocketData(data=log_dict, operation=SocketOperation.CREATE)
-        )
+    @abstractmethod
+    async def notify_create(self, log_body: LogBody):
+        pass
 
-    def notify_update(self, log_body: LogBody):
-        log_dict = log_body.model_dump(mode="json")
-        if isinstance(log_body, ActionLogBody):
-            log_dict["response"] = self._message_pool.get_message_by_id(log_body.response).model_dump(mode="json")
-        self._socket_cache.append(
-            SocketData(data=log_dict, operation=SocketOperation.UPDATE)
-        )
+    @abstractmethod
+    async def notify_update(self, log_body: LogBody):
+        pass
 
+    @abstractmethod
     async def stream_sockets(self, websocket: WebSocket) -> bool:
-        cur = 0
-        closed = False
-        try:
-            while not self._stopped:
-                if cur >= len(self._socket_cache):
-                    await asyncio.sleep(0.001)
-                else:
-                    await websocket.send_json(self._socket_cache[cur].model_dump_json())
-                    await asyncio.sleep(0.001)
-                    cur += 1
-            for socket in self._socket_cache[cur:]:
-                await websocket.send_json(socket.model_dump_json())
-        except:
-            closed = True
-        return closed
+        pass
 
     def stop(self):
         self._stopped = True
